2dbaca257f0b3d6be172bbf9b4ab1ff20b7a41a5
alex-killer <onefrank@bk.ru>
2022-03-17 17:25:10 +0700

    Initial commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => Category::get()->random()->id,
+            'user_id' => User::get()->random()->id,
+            'tag_id' => Tag::get()->random()->id,
+            'title' => $this->faker->word,
+            'content' => $this->faker->text(100),
+            'is_published' => rand(0, 1),
+
+        ];
+    }
+}
---
+++ database/migrations/2022_03_17_082740_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id')->unsigned();
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->unsignedBigInteger('tag_id')->unsigned();
+
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->boolean('is_published')->default(false);
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_id', 'post_category_fk')->on('categories')->references('id');
+            $table->foreign('user_id', 'post_user_fk')->on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
